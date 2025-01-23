ae4a9fedf358ff416c5782b99cbe1e194aa44337
SheYuting <e1032467@u.nus.edu>
2025-01-23 17:54:40 +0800

    added greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        String logo = "Duke";
+        System.out.println("Hello! I'm " + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again");
+        System.out.println("____________________________________________________________");
     }
 }
