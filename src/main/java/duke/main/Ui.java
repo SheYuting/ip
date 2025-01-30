94087a94168884e5fc65d576552b4061c9951aff
SheYuting <e1032467@u.nus.edu>
2025-01-31 03:58:16 +0800

    Tweak the code to comply with checkstyle

--- src/main/java/duke/main/Ui.java
+++ src/main/java/duke/main/Ui.java
@@ -8,7 +8,7 @@ public class Ui {
     public void showWelcome() {
         System.out.println("Hello! I'm main.Duke");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        showLine();
     }
 
     public String readCommand() {
@@ -17,20 +17,16 @@ public class Ui {
 
     public void showError(String message) {
         System.out.println("OOPS!!! " + message);
-        System.out.println("____________________________________________________________");
+        showLine();
     }
 
     public void showLine() {
         System.out.println("____________________________________________________________");
     }
 
-    public void showLoadingError() {
-        System.out.println("There was an error loading the tasks.");
-    }
-
     public void showGoodbye() {
         System.out.println("Bye. Hope to see you again!");
-        System.out.println("____________________________________________________________");
+        showLine();
     }
 
     public void showTaskList(ArrayList<Task> tasks) {
@@ -38,6 +34,7 @@ public class Ui {
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println((i + 1) + ". " + tasks.get(i));
         }
+        showLine();
     }
 
     public void showTaskCount(int count) {
@@ -46,6 +43,7 @@ public class Ui {
 
     public void addSuccess(Task task) {
         System.out.println("Got it. I've added this task:\n  " + task);
+        showLine();
     }
 
     public void printMessage(String message) {
