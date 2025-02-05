26684d750046bf1beac44f56baacbc351f1fd987
SheYuting <e1032467@u.nus.edu>
2025-02-05 18:36:16 +0800

    Add CheckStyle in build.gradle

--- src/main/java/duke/main/Duke.java
+++ src/main/java/duke/main/Duke.java
@@ -3,26 +3,35 @@ package duke.main;
 import duke.command.Command;
 import duke.exception.DukeException;
 import duke.main.Storage;
-
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 /**
  * Duke is a chatbot that helps users manage their tasks.
  */
-public class Duke {
+public class Duke extends Application {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+    private static final String DEFAULT_FILE_PATH = "./data/duke.txt";
 
     /**
      * The main entry point of the Duke application.
      * Loads tasks from storage and processes user commands in a loop.
      *
-     * @param args Command line arguments (not used).
      */
-    public static void main(String[] args) throws DukeException {
-        new Duke("./data/duke.txt").runDuke();
+
+    public Duke() throws DukeException {
+        this(DEFAULT_FILE_PATH);
     }
 
-    private Duke(String filePath) throws DukeException {
+    public Duke(String filePath) throws DukeException {
         this.ui = new Ui();
         this.storage = new Storage(filePath);
         this.tasks = storage.loadTasks(ui);
